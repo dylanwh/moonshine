d4e219a99f80223e43c4f871e0061dd56df43e1a
Bryan Donlan <bdonlan@gmail.com>
2007-05-29 02:24:00 +0000

    fix modeline and indent in term.c
    darcs-hash:20070529022410-af6ff-c4447f4fb1bcb065c4965d572edfd7f881438860.gz

--- term.c
+++ term.c
@@ -1,4 +1,4 @@
-/* vim: set ft=c noexpandtab ts=4 sw=4 tw=80 */
+/* vim: set ft=c noexpandtab ts=4 sw=4 tw=80 : */
 #include <glib.h>
 #include <slang.h>
 #include <stdlib.h>
@@ -16,7 +16,7 @@ void term_init(void)
 	// SLang_set_abort_signal(on_abort);
 	SLsmg_init_smg ();
 	SLutf8_enable (-1);
-    SLsmg_utf8_enable(-1);
+	SLsmg_utf8_enable(-1);
 	SLsmg_refresh();
 	/* SLsmg_embedded_escape_mode(1); */
 
