5a9dd71b3aa7ab4dce085e779ad0d7d74568b35f
Bryan Donlan <bdonlan@gmail.com>
2007-05-29 01:43:00 +0000

    new haver protocol parser
    darcs-hash:20070529014322-af6ff-5ff803c7dcacbc253ef10dac03f3aab0720fd2ad.gz

--- protocol.c
+++ protocol.c
@@ -1,45 +1,160 @@
-/* vim: set ft=c noexpandtab ts=4 sw=4 tw=80 */
-#include "config.h"
+/* vim: set ft=c.doxygen noexpandtab ts=4 sw=4 tw=80 */
+ 
 #include "protocol.h"
 
-const GSList *haver_parse(GString *s)
-{
-	GSList *result = NULL;
-	guint start = 0;
-	gchar *str = s->str;
-
-	for (guint i = 0; str[i] != '\0'; i++) {
-		if (str[i] == '\t') {
-			GString *buf = g_string_new_len(&str[start], i - start);
-			result = g_slist_prepend(result, buf);
-			start = i+1;
-		} else if (str[i+1] == '\0') {
-			GString *buf = g_string_new_len(&str[start], i + 1 - start);
-			result = g_slist_prepend(result, buf);
+GString *haver_strerror(int err) {
+	switch(err) {
+		case HAVER_NO_ERROR: return g_string_new("No error");
+		case HAVER_UNKNOWN_ESCAPE: return g_string_new("Unknown escape sequence");
+		case HAVER_TRUNCATED_ESCAPE: return g_string_new("Truncated escape sequence");
+		case HAVER_TRUNCATED_INPUT: return g_string_new("Truncated input");
+		case HAVER_BAD_UTF8: return g_string_new("Bad utf8 sequence in input");
+		case HAVER_BAD_CHARS: return g_string_new("Illegal characters in input");
+		case HAVER_EMPTY_ARR: return g_string_new("Empty list of strings in encode");
+		default: {
+			GString *str = g_string_new("");
+			g_string_printf(str, "Invalid error code in haver_strerror(%d)", err);
+			return str;
 		}
 	}
-	
-	return g_slist_reverse(result);
 }
 
-const GString *haver_format(GSList *msg)
-{
-	GString *buf = g_string_new("");
-	void each(GString *str, GString *buf) {
-		if (buf->len != 0)
-			g_string_append_c(buf, '\t');
-		g_string_append(buf, str->str);
+GPtrArray *haver_decode(const gchar *line, const gchar **remain, int *error) {
+	g_assert(line);
+
+	int dummy_err;
+	const gchar *dummy_remain;
+	GPtrArray *temp_a = NULL;
+	GString *cur_s = NULL;
+	if (!error) error = &dummy_err;
+	if (!remain) remain = &dummy_remain;
+	*error = HAVER_NO_ERROR;
+
+	if (!g_utf8_validate(line, -1, NULL)) {
+		*error = HAVER_BAD_UTF8;
+		return NULL;
 	}
-	g_slist_foreach(msg, (GFunc) each, buf);
-	return buf;
-}
 
-void haver_msg_free(GSList *msg)
-{
-	void each(GString *str, UNUSED int foo)
-	{
+	temp_a = g_ptr_array_new();
+	while (*line) {
+		if (!cur_s) {
+			cur_s = g_string_new("");
+			g_ptr_array_add(temp_a, cur_s);
+		}
+		switch (*line) {
+			case '\n':
+			case '\r':
+				while (*line == '\n' || *line == '\r') line++;
+				*remain = line;
+				return temp_a;
+			case '\t':
+				cur_s = NULL;
+				break;
+			case '\x1F':
+				line++;
+				switch (*line) {
+					case '\0':
+					case '\t':
+					case '\n':
+					case '\r':
+						*error = HAVER_TRUNCATED_ESCAPE;
+						goto error_out;
+					case 'n': g_string_append_c(cur_s, '\n'); break;
+					case 'r': g_string_append_c(cur_s, '\r'); break;
+					case 't': g_string_append_c(cur_s, '\t'); break;
+					case 'e': g_string_append_c(cur_s, '\x1F'); break;
+					default:
+						*error = HAVER_UNKNOWN_ESCAPE;
+						goto error_out;
+				}
+				break;
+			default:
+				g_string_append_c(cur_s, *line);
+		}
+		line++;
+	}
+	*error = HAVER_TRUNCATED_INPUT;
+	goto error_out;
+
+error_out:
+	g_assert(*error != HAVER_NO_ERROR);
+	void free_one(gpointer str, UNUSED gpointer user_data) {
 		g_string_free(str, TRUE);
 	}
-	g_slist_foreach(msg, (GFunc)each, NULL);
-	g_slist_free(msg);
+
+	if (temp_a) {
+		g_ptr_array_foreach(temp_a, free_one, NULL);
+		g_ptr_array_free(temp_a, TRUE);
+	}
+	return NULL;
+}
+
+struct enc_info {
+	int *error;
+	GString *accum;
+};
+
+GString *haver_encode(const GPtrArray *line, int *error) {
+	g_assert(line);
+
+	int dummy_error;
+	if (!error) error = &dummy_error;
+	*error = HAVER_NO_ERROR;
+
+	int est_size = 2;
+	GString *str = NULL;
+
+	if (line->len == 0) {
+		*error = HAVER_EMPTY_ARR;
+		goto error_out;
+	}
+
+	void tally(gpointer v_str, gpointer v_est) {
+		int *est = v_est;
+		GString *str = v_str;
+		est += str->len + 1;
+	}
+	g_ptr_array_foreach((GPtrArray *)line, tally, &est_size);
+
+	str = g_string_sized_new(est_size);
+	void append(gpointer v_str, gpointer v_info) {
+		GString *str = v_str;
+		struct enc_info *info = v_info;
+
+		if (*info->error) return;
+		if (!g_utf8_validate(str->str, -1, NULL)) {
+			*info->error = HAVER_BAD_UTF8;
+			return;
+		}
+
+		gchar *p = str->str;
+		while (*p) {
+			switch (*p) {
+				case '\n': g_string_append(info->accum, "\x1Fn"); break;
+				case '\r': g_string_append(info->accum, "\x1Fr"); break;
+				case '\t': g_string_append(info->accum, "\x1Ft"); break;
+						   /* XXX: \x1Fe breaks here, is there a less ugly
+							* way for it to be interpreted properly?
+							*/
+				case '\x1F': g_string_append(info->accum, "\x1F" "e"); break;
+				default: g_string_append_c(info->accum, *p); break;
+			}
+			p++;
+		}
+		g_string_append_c(info->accum, '\t');
+	}
+	struct enc_info info = { error, str };
+	g_ptr_array_foreach((GPtrArray *)line, append, &info);
+	if (*error != HAVER_NO_ERROR)
+		goto error_out;
+
+	g_string_truncate(str, str->len - 1);
+	g_string_append(str, "\x0D\x0A");
+
+	return str;
+error_out:
+	g_assert(*error != HAVER_NO_ERROR);
+	
+	if (str) g_string_free(str, TRUE);
+	return NULL;
 }
--- protocol.h
+++ protocol.h
@@ -1,10 +1,46 @@
 /* vim: set ft=c noexpandtab ts=4 sw=4 tw=80 */
-#ifndef __MOONSHINE_PROTOCOL_H__
-#define __MOONSHINE_PROTOCOL_H__
+#ifndef __SPOON_PROTOCOL_H__
+#define __SPOON_PROTOCOL_H__
 #include <glib.h>
+#include "config.h"
 
-const GSList *haver_parse(GString *buf) G_GNUC_PURE;
-const GString *haver_format(GSList *msg) G_GNUC_PURE;
-void haver_msg_free(GSList *msg);
+#define HAVER_NO_ERROR			0 ///< Decode was successful
+#define HAVER_UNKNOWN_ESCAPE	1 ///< Unknown escape code in input
+#define HAVER_TRUNCATED_ESCAPE	2 ///< Truncated escape (eg, ESC NL or ESC TAB)
+#define HAVER_TRUNCATED_INPUT	3 ///< No newline at end of input
+#define HAVER_BAD_UTF8			4 ///< Input is invalid unicode
+#define HAVER_BAD_CHARS			5 ///< Input contains illegal characters
+#define HAVER_EMPTY_ARR			6 ///< An array of zero elements was passed to encode
+
+/** \brief Returns a human-readable form of a haver error code
+ *
+ * \param err The error code in question
+ * \return A new GString containing a human-readable error message
+ */
+GString *haver_strerror(int err);
+
+/** \brief Breaks a haver protocol message into its tab-delimited items,
+ *   replacing escape sequences.
+ *
+ * \param line The null-delimited string to decode
+ * \param remain If not NULL, a pointer to a location in which to store the
+ *   location of beginning of the next line (or the null-delimiter, if there
+ *   is no next line)
+ * \param error If not NULL, a pointer to a location in which to store the error
+ *   code.
+ * \return A GPtrArray of GStrings in UTF8 coding, if successful, or NULL
+ *   otherwise.
+ */
+GPtrArray *haver_decode(const gchar *line, const gchar **remain, int *error);
+
+/** \brief Encodes a haver protocol message into a network-ready format.
+ *
+ * \param line A GPtrArray of GStrings in UTF-8 coding.
+ * \param error If not NULL, a pointer to a location in which to store the
+ *   error code.
+ * \return A GString in network-ready format if successful, or NULL otherwise.
+ */
+GString *haver_encode(const GPtrArray *line, int *error);
 
 #endif
+
