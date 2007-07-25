7bd1c7974c36defb8dd9b6b422bd26ffa0f5d989
dylan <dylan@hardison.net>
2007-07-25 18:18:00 +0000

    Implemented key binding functions, in lua!
    darcs-hash:20070725181802-26fed-32de6c9ee580b1abfb84a9bfd8c4d930f5c7b725.gz

--- lua/boot.lua
+++ lua/boot.lua
@@ -1,11 +1,55 @@
 -- Eventually, with a saner keybinding system (bd_?)
 -- bind() and unbind() can be C primitives.
-function bind(spec, f)
-	local i = keymap:define(spec)
-	keymap.event[i] = f
+
+do
+	local keys = {}
+	local last_key = nil
+
+	function bind(spec, f)
+		local k = keys
+		local function index(s, i)
+			return s:sub(i+1, i+1)
+		end
+		-- make_keyspec translates ^A into 0, etc.
+		spec = make_keyspec(spec)
+		last = spec:len() - 1
+		for i = 0, last, 1 do
+			local x = index(spec, i)
+			if i == last then
+				k[x] = f
+			else
+				k[x] = {}
+				k = k[x]
+			end
+		end
+	end
+
+	function on_input(key)
+		if type(last_key) == 'table' then
+			last_key = last_key[key]
+		else
+			last_key = keys[key]
+			if not last_key then
+				on_keypress(key)
+			end
+		end
+		if type(last_key) == 'function' then
+			last_key()
+			last_key = nil
+		end
+	end
+
+	function on_input_reset()
+		last_key = nil
+	end
+
+	function on_keypress(key)
+		-- entry:keypress(key)
+	end
 end
 
-function unbind(spec)
-	local i = keymap:undefine(spec)
-	keymap.event[i] = nil
-end
+bind("^[[D", function () print("left") end)
+bind("^C", os.exit)
+bind("·", function () on_input(".") end)
+
+
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 void on_signal(int fd, short event, void *arg)
 {
@@ -13,11 +14,35 @@ void on_signal(int fd, short event, void *arg)
 
 void on_input(int fd, short event, void *arg)
 {
-	Entry *entry = arg;
-	gunichar c = term_getkey();
-	entry_key(entry, c);
-	entry_render(entry, 2);
-	term_refresh();
+	LuaState *L = arg;
+	do {
+		gunichar c = term_getkey();
+		char buf[8];
+		g_unichar_to_utf8(c, buf);
+		moon_call(L, "on_input", "s", buf);
+	} while (SLang_input_pending(-20));
+
+	moon_call(L, "on_input_reset", "");
+}
+
+/* make_keyspec turns strings like "^A" into "\001", and so on. */
+int make_keyspec(LuaState *L)
+{
+	const char *str = luaL_checkstring(L, 1);
+	int len = strlen(str);
+	GString *buf = g_string_sized_new(len);
+
+	for (int i = 0; i < len; i++) {
+		if (str[i] == '^') {
+			g_string_append_c(buf, str[i + 1] ^ 64);
+			i++;
+		} else {
+			g_string_append_c(buf, str[i]);
+		}
+	}
+	lua_pushstring(L, buf->str);
+	g_string_free(buf, TRUE);
+	return 1;
 }
 
 int main(int argc, char *argv[])
@@ -30,9 +55,13 @@ int main(int argc, char *argv[])
 
 	event_init();
 	term_init();
+	atexit(term_reset);
 
 	entry = entry_new();
 	L = moon_init();
+	lua_pushcfunction(L, make_keyspec);
+	lua_setglobal(L, "make_keyspec");
+
 	//keymap_init(L);
 	if (luaL_dofile(L, "lua/boot.lua")) {
 		const char *err = lua_tostring(L, -1);
@@ -45,11 +74,11 @@ int main(int argc, char *argv[])
 	event_set(&sigterm, SIGTERM, EV_SIGNAL|EV_PERSIST, on_signal, NULL);
 	event_add(&sigterm, NULL);
 
-	event_set(&input, fileno(stdin), EV_READ|EV_PERSIST, on_input, entry);
+	event_set(&input, fileno(stdin), EV_READ|EV_PERSIST, on_input, L);
 	event_add(&input, NULL);
 
 	event_dispatch();
 
-	term_reset();
+	//term_reset();
 	return 0;
 }
