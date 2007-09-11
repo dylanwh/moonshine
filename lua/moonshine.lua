27b0299f7567eac769f2d47c21ad5200227aef0d
dylan <dylan@hardison.net>
2007-09-11 15:51:00 +0000

    finished client wrapper.
    darcs-hash:20070911155117-26fed-c5c0363deee4e8ce7021201ab3bb6b4e05f4ecd7.gz

--- lua/moonshine.lua
+++ lua/moonshine.lua
@@ -7,9 +7,19 @@ require "ui"
 require "cmd"
 require "config"
 
+
+declare "on_client"
+function on_client(client, event, msg)
+	ui:print("%1%| %2", event, msg)
+	ui:render()
+end
+
+declare "c"
 declare "on_boot"
 function on_boot()
 	ui:render()
+	c = Client.new("localhost", 7575, on_client)
+	c:connect()
 end
 
 declare "on_shutdown"
