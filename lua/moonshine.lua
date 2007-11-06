8b4325cc73b8658b26be00d0fa590dc6c163d606
dylan <dylan@hardison.net>
2007-11-06 03:15:00 +0000

    redefine print builtin to use current window's buffer.
    We should have this use the 'status' window, but windows do not yet have names.
    
    darcs-hash:20071106031503-26fed-97d9e1b08ebbf7a23221a239f5c7e20e12485060.gz

--- lua/moonshine.lua
+++ lua/moonshine.lua
@@ -11,4 +11,41 @@ function quit_hook()
 	ui.print("Shutdown: %1", "bob")
 end
 
+function string.join(delimiter, list)
+  	local len = getn(list)
+  	if len == 0 then 
+    	return "" 
+  	end
+  	local string = list[1]
+  	for i = 2, len do 
+    	string = string .. delimiter .. list[i] 
+  	end
+  	return string
+end
+
+-- Split text into a list consisting of the strings in text,
+-- separated by strings matching delimiter (which may be a pattern). 
+-- example: strsplit(",%s*", "Anna, Bob, Charlie,Dolores")
+function string.split(delimiter, text)
+  	local list = {}
+  	local pos = 1
+  	if strfind("", delimiter, 1) then -- this would result in endless loops
+    	error("delimiter matches empty string!")
+  	end
+  	while 1 do
+    	local first, last = strfind(text, delimiter, pos)
+    	if first then -- found?
+      		tinsert(list, strsub(text, pos, first-1))
+      		pos = last+1
+    	else
+      		tinsert(list, strsub(text, pos))
+      		break
+    	end
+  	end
+  	return list
+end
+
+function print(...)
+	ui.print("%1", string.join("", arg))
+end
 
