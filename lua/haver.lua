b730bdb2584eafa1bfe4147888b62f5f4814c398
Bryan Donlan <bd@fushizen.net>
2007-11-15 01:27:00 +0000

    Fix typo in unrecognized haver command message
    darcs-hash:20071115012711-7b87e-e244e51dd316cbd85268dd5c86f7fad86fd12f17.gz

--- lua/haver.lua
+++ lua/haver.lua
@@ -46,7 +46,7 @@ function Haver:on_event(event, ...)
 			if self[cmd] then
 				self[cmd](self, unpack(msg))
 			else
-				screen:debug("Unknown command: %1i (%2)", cmd, join(", ", {...}))
+				screen:debug("Unknown command: %1 (%2)", cmd, join(", ", {...}))
 			end
 		end
 	elseif event == 'eof' then
