a3a45a1effaaa31332bce11f8f37094be2f935b3
Dylan William Hardison <dylan@hardison.net>
2009-05-18 22:17:25 -0400

    added skeleton protocol prototype.

--- runtime/moonshine/protocol.lua
+++ runtime/moonshine/protocol.lua
@@ -1,82 +1,34 @@
 local Object = require "moonshine.object"
+local Protocol = Object:new { }
 
--- A protocol should define the following methods:
--- * join(room)
--- * part(room)
--- * usersof(room)
--- * msg(target, kind, msg)
--- * connect(hostname, port)
--- * disconnect()
--- * canonize_name(type, name)
---
--- The following fields are optional:
--- * username (default: $USER)
-local Protocol = Object:clone {
-	__type = "Protocol",
-	username = os.getenv "USER",
-}
+function Protocol:__init()
+	assert(self.host)
+	assert(self.port)
 
-function Protocol:canonize_name(type, name)
-	return name:lower()
+	self.client = client:new(self.host, self.port, function(event, ...)
+		self[ "on_" .. event](self, ...)
+	end)
 end
 
--- Returns a table which canonizes its keys according to the protocol's own
--- canonize_name() method. By default, this yields a case-insensitive table.
-function Protocol:magic_table(type)
-	local t = {}
-	local mt = {}
-	function mt.__index(t, key)
-		return rawget(t, self:canonize_name(type, key))
-	end
-	function mt.__newindex(t, key, val)
-		return rawset(t, self:canonize_name(type, key), val)
-	end
-	setmetatable(t, mt)
-	return t
+function Protocol:on_connect()
 end
 
-function Protocol:connect(hostname, port)
-	self.hostname = hostname or self.hostname
-	self.port     = port     or self.port
-
-	connect_hook(self)
-	net.connect(self.hostname, self.port, self:callback "on_connect")
+function Protocol:on_close()
 end
 
-function Protocol:disconnect()
-	if self.handle then
-		self.handle:close()
-	end
-	disconnect_hook(self)
+function Protocol:on_read(line)
 end
 
-function Protocol:on_connect(fd, ...)
-	if fd then
-		self.reader = LineReader:new()
-		self.handle = Handle:new(fd, self:callback "on_event")
-		connected_hook(self, fd)
-	else
-		return connection_error_hook(...)
+function Protocol:on_write()
+end
+
+function Protocol:on_timeout()
+end
+
+for i, name in ipairs { "read", "readn", "readline", "connect", "disconnect", "is_connected", "write" } do
+	Protocol[name] = function(self, ...)
+		return self.client[name](self.client, ...)
 	end
 end
 
-function Protocol:on_event(event, errtype, errmsg)
-	if event == 'input' then
-		local str = self.handle:read()
-		if str then
-			for line in self.reader:lines(str) do
-				self:on_readline(line)
-			end
-		else
-			screen:debug("eof")
-			self:disconnect()
-		end
-	elseif event == 'error' then
-		if errtype == 'write' then
-			screen:debug("write error: %1", errmsg)
-		else
-			screen:debug("error: %1", errtype)
-		end
-		self:disconnect()
-	end
-end
+return Protocol
