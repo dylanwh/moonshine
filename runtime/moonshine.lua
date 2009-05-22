3fa07e3563311140068517b9b3020e09365dd524
Dylan William Hardison <dylan@hardison.net>
2009-05-21 23:14:53 -0400

    removed outdated ideas from two days ago.

--- runtime/moonshine.lua
+++ runtime/moonshine.lua
@@ -4,19 +4,6 @@ require "moonshine.log"
 
 local loop = require "moonshine.loop"
 
-function cmd_go()
-	protocol.connect('irc', { hostname = os.getenv('IRC_HOST'), port = tonumber(os.getenv('IRC_PORT')) })
-
-end
-
-function cmd_join()
-	irc:join('#lobby')
-end
-
-function cmd_say(text)
-	irc:message( { type = 'room', name = 'lobby' }, 'say', text)
-end
-
 emit "startup"
 loop.run()
 emit "shutdown"
