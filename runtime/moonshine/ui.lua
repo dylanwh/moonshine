e870405256f20344ef952aa7766a0707de641c00
Dylan William Hardison <dylan@hardison.net>
2009-05-22 11:42:23 -0400

    changed keybindings again.

--- runtime/moonshine/ui.lua
+++ runtime/moonshine/ui.lua
@@ -10,54 +10,48 @@ term.defcolor("statusimportant", "brightmagenta", "blue")
 term.defcolor("self", "white", "default")
 
 local keymap = require "moonshine.keymap"
-local loop   = require "moonshine.loop"
 local screen = require "moonshine.ui.screen.main"
+local shell  = require "moonshine.shell"
 
 --{{{ keymap stuff.
-kb_screen_redraw            = screen:callback "render"
-kb_window_goto              = screen:callback "goto"
-kb_entry_submit             = screen:callback "submit"
-kb_entry_backspace          = screen:callback "backspace"
-kb_buffer_scroll_up         = screen:callback "scroll_up"
-kb_buffer_scroll_down       = screen:callback "scroll_down"
-kb_entry_move_left          = screen:callback "move_left"
-kb_entry_move_right         = screen:callback "move_right"
-kb_entry_move_home          = screen:callback "move_home"
-kb_entry_move_end           = screen:callback "move_end"
-kb_entry_history_prev       = screen:callback "history_backward"
-kb_entry_history_next       = screen:callback "history_forward"
-kb_entry_delete_word        = screen:callback "word_delete"
-kb_entry_move_left_by_word  = screen:callback "word_left"
-kb_entry_move_right_by_word = screen:callback "word_right"
-kb_quit                     = loop.quit
+kb_redraw        = screen:callback "render"
+kb_submit        = screen:callback "submit"
+kb_backspace     = screen:callback "backspace"
+kb_page_up       = screen:callback "scroll_up"
+kb_page_down     = screen:callback "scroll_down"
+kb_left          = screen:callback "move_left"
+kb_right         = screen:callback "move_right"
+kb_home          = screen:callback "move_home"
+kb_end           = screen:callback "move_end"
+kb_up            = screen:callback "history_backward"
+kb_down          = screen:callback "history_forward"
+kb_delete_word   = screen:callback "word_delete"
+kb_left_by_word  = screen:callback "word_left"
+kb_right_by_word = screen:callback "word_right"
+kb_command       = shell.eval
 
-keymap.bind("^[[A",    "entry_history_prev")
-keymap.bind("^[[B",    "entry_history_next")
-keymap.bind("^[[C",    "entry_move_right")
-keymap.bind("^[[D",    "entry_move_left")
-keymap.bind("^[[H",    "entry_move_home")
-keymap.bind("^[[F",    "entry_move_end")
+keymap.bind("^[[A",    "up")
+keymap.bind("^[[B",    "down")
+keymap.bind("^[[C",    "right")
+keymap.bind("^[[D",    "left")
+keymap.bind("^[[H",    "home")
+keymap.bind("^[[F",    "end")
 
-keymap.bind("^A",      "entry_move_home")
-keymap.bind("^[[7~",   "entry_move_home")
-keymap.bind("^[[1~",   "entry_move_home")
-keymap.bind("^[[8~",   "entry_move_end")
-keymap.bind("^[[4~",   "entry_move_end")
-keymap.bind("^[[5~",   "buffer_scroll_up")
-keymap.bind("^[[6~",   "buffer_scroll_down")
-keymap.bind("^[[1;5D", "entry_move_left_by_word")
-keymap.bind("^[[1;5C", "entry_move_right_by_word")
-keymap.bind("^W",      "entry_delete_word")
-keymap.bind("^?",      "entry_backspace")
-keymap.bind("^H",      "entry_backspace")
-keymap.bind("^M",      "entry_submit")
-keymap.bind("^C",      "quit")
-keymap.bind("^L",      "screen_redraw")
-
-for i = 1, 9 do
-	keymap.bind("^[" .. i, "window_goto", i)
-end
---}}}
+keymap.bind("^A",      "home")
+keymap.bind("^[[7~",   "home")
+keymap.bind("^[[1~",   "home")
+keymap.bind("^[[8~",   "end")
+keymap.bind("^[[4~",   "end")
+keymap.bind("^[[5~",   "page_up")
+keymap.bind("^[[6~",   "page_down")
+keymap.bind("^[[1;5D", "left_by_word")
+keymap.bind("^[[1;5C", "right_by_word")
+keymap.bind("^W",      "delete_word")
+keymap.bind("^?",      "backspace")
+keymap.bind("^H",      "backspace")
+keymap.bind("^M",      "submit")
+keymap.bind("^C",      "command", "/quit")
+keymap.bind("^L",      "redraw")
 
 function on_unknown_hook(name)
 	screen:debug("unknown hook: " .. name)
