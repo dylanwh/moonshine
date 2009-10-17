97c3307a1d1ff12ab1c5289a3e1b02a6b2296c49
Dylan William Hardison <dylan@hardison.net>
2009-10-17 02:01:18 -0400

    use more method-style rather than module-style.

--- runtime/moonshine/shell.lua
+++ runtime/moonshine/shell.lua
@@ -5,23 +5,25 @@
 -- shell.eval('/foo bar baz')
 -- shell.call('foo', 'bar baz')
 --
--- shell.register(name, {
+-- shell.define(name, {
 --     func = function(text) assert(text == 'bar baz') end,
 -- }
 --
--- shell.require(name): shortcut for shell.register(name, require('moonshine.shell.' .. name))
+-- shell.require(name): shortcut for shell.define(name, require('moonshine.shell.' .. name))
 --
--- TODO: document spec argument to shell.register.
+-- TODO: document spec argument to shell.define.
 local parseopt = require "moonshine.parseopt"
+
 local M        = {}
-local cmd      = {}
+local CMD      = {}
 
 function M.call(name, arg)
-    local func = cmd[name]
+    local func = CMD[name]
+
     if not func then
         local ok, errmsg = pcall(M.require, name)
         if ok then
-            func = cmd[name]
+            func = CMD[name]
         elseif not errmsg:match("module 'moonshine.shell." .. name .."' not found:") then
             run_hook('shell error', errmsg)
             return false
@@ -43,11 +45,11 @@ function M.call(name, arg)
 end
 
 function M.eval(line)
-    local name, pos = string.match(line, "^/([%w_]+)()")
+    local name, pos = line:match("^/([%w_]+)()")
     local arg
     if name then
-        name = string.lower(name)
-        arg  = string.sub(line, pos+1)
+        name = name:lower()
+        arg  = line:sub(pos+1)
     else
         name = "say"
         arg  = line
@@ -56,26 +58,26 @@ function M.eval(line)
     return M.call(name, arg)
 end
 
-function M.register(name, def)
-    local spec  = def.spec
-    local run   = def.run or def.func
+function M.define(name, option)
+    local spec = option.spec
+    local run  = option.run
 
     assert(name, "name required")
     assert(run,  "run field required")
 
     if spec then
         local parser = parseopt.build_parser( unpack(spec) )
-        cmd[name] = function(text)
+        CMD[name] = function(text)
             run( parser(text) )
         end
     else
-        cmd[name] = run
+        CMD[name] = run
     end
 end
 
 function M.require(name)
     local mod = require("moonshine.shell." .. name)
-    M.register(name, mod)
+    M.define(name, mod)
     return mod
 end
 
