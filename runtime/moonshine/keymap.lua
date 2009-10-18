3c1b4acdfbe55df0a14da5bbd1d59d0540326eaa
Dylan William Hardison <dylan@hardison.net>
2009-10-18 18:25:37 -0400

    keymap is now a class.

--- runtime/moonshine/keymap.lua
+++ runtime/moonshine/keymap.lua
@@ -1,57 +1,46 @@
 -- API
--- keymap.bind('^[1', 'window_goto', 1)
+-- keymap:bind('^[1', '/window goto 1')
 --
--- keymap.process('^[')
--- keymap.process('1')
+-- keymap:keypress('^[')
+-- keymap:keypress('1')
 --
--- Results of above two calls to process():
--- kb_window_goto(1)
-local M       = {}
-
+-- Results of above two calls to keypress():
+-- /window goto 1
 local bit     = require "bit"
-local Tree    = require "moonshine.tree"
-local mapping = Tree:new()
-local cmd     = {}
-local keybuf  = ""
 
-local function keyspec(spec)--{{{
-    return spec:gsub("%^(.)", function(c)
+local function keyspec(spec)
+    local function ctrl(c)
         return string.char(bit.bxor(string.byte(string.upper(c)), 64))
-    end)
-end--}}}
+    end
+    return spec:gsub("%^(.)", ctrl)
+end
 
-function M.bind(spec_, name, ...)--{{{
-    local spec = keyspec(spec_)
-    local extra = { ... }
-    mapping:insert(spec, function()
-        M.invoke(name, unpack(extra))
-    end)
+local Keymap = new "moonshine.object"
 
-    mapping:find(spec)
-end--}}}
+function Keymap:__init(attr)
+    self._shell  = attr.shell or new "moonshine.shell"
+    self._tree   = new "moonshine.tree"
+    self._keybuf = ""
+end
 
-function M.register(name, func)--{{{
-    assert(cmd[name] == nil)
-    cmd[name] = func
-end--}}}
+function Keymap:keypress(key)
+    local tree  = self._tree
+    local shell = self._shell
+    self._keybuf = self._keybuf .. key
 
-function M.process(key)--{{{
-    keybuf = keybuf .. key
-
-    local found_key, func, index, dirn = mapping:find_near(keybuf)
-
+    local found_key, text, index, dirn = tree:find_near(self._keybuf)
     if found_key == nil then
         -- No keys defined??
-        return
+        return false
     end
 
     if dirn == 0 then
-        func()
-        keybuf = ""
-        return
+        self._keybuf = ""
+        shell:eval(text)
+        return true
     elseif dirn > 0 then
         index = index + 1
-        found_key, func = mapping:lookup_index(index)
+        found_key, text = tree:lookup_index(index)
     end
 
     if found_key == nil then
@@ -59,20 +48,18 @@ function M.process(key)--{{{
         found_key = ""
     end
 
-    if string.sub(found_key, 1, string.len(keybuf)) ~= keybuf then
+    if string.sub(found_key, 1, string.len(self._keybuf)) ~= self._keybuf then
         -- not a prefix of anything
-        run_hook("keypress", key)
-        keybuf = ""
+        self._keybuf = ""
+        return false
     else
         -- prefix of something, keep going
+        return true
     end
-end--}}}
+end
 
-function M.invoke(name, ...)--{{{
-    local func = cmd[name]
-    if func then 
-        func(...)
-    end
-end--}}}
+function Keymap:bind(spec, text)
+    self._tree:insert(keyspec(spec), text)
+end
 
-return M
+return Keymap
