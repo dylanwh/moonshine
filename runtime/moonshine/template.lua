172b47a4230d9b03765076527a566e7a492d73c8
Dylan William Hardison <dylan@hardison.net>
2010-03-28 01:10:31 -0400

    I spent way too much time on the templating system.

--- runtime/moonshine/template.lua
+++ runtime/moonshine/template.lua
@@ -1,121 +1,11 @@
---[[
-    %foo           is a macro call
-    %{foo}         is a macro call
-    %{foo bar baz} is a macro call with arguments.
-
-    Arguments can be:
-        * macro calls
-        * sugar (described below)
-        * quoted strings ("foo" or 'bar')
-        * bare words (foo, foo123, foo_123, foo_bar, _)
-        * numbers (5, 8.4, .3)
-
-    Sugar:
-        * %1         is short for %{param 1}
-        * %%         is a literal '%'
-        * %|         is short for %{const '|'}
-        * %(TEXT)    is special, it causes TEXT to be evaluated as if it was outside of any macro call.
-
-    Literals: Anything that does not begin with a '%' is a literal.
-]]
-require "moonshine.prelude" -- for new()
-
--- {{{ lpeg imports
-local lpeg          = require "lpeg"
-local P, R, S, V    = lpeg.P, lpeg.R,  lpeg.S,  lpeg.V
-local C, Ct, Cc, Cs = lpeg.C, lpeg.Ct, lpeg.Cc, lpeg.Cs
-local match         = lpeg.match
--- }}}
-
--- {{{ grammar
-local letter  = R('AZ','az') + '_'
-local digit   = R '09'
-local space   = S " \t\r\n"
-local sp0     = space^0
-local sp1     = space^1
-
-local number  = C(digit^1 * '.' * digit^1)
-              + C(digit^1)
-              + C('.' * digit^1)
-local word    = letter * (letter + digit)^0
-local quote   = function(chr)
-    local any = (P ("\\" .. chr) / chr)
-              + (P "\\\\" / "\\")
-              + (P(1) - chr)
-    return chr * Cs( any^0 ) * chr
-end
-
-local quoted  = quote([["]]) + quote([[']])
-local name    = C( word )
-local literal = C( (P(1) - '%' )^1 )
-
-local top   = {TOP=1} -- special value to represent "top" opcode.
-local eval  = {EVAL= 1} -- same, for code injection.
-local template = P {
-    'top',
-    top      = Ct( Cc(top) * (literal + V 'escape')^1 ) * -1,
-    inject   = '(' * sp0 * C(((1 - S '()') + V 'inject')^0) * sp0 * ')',
-    escape   = V 'macro' + V 'eval' + V 'param' + V 'const',
-    eval     = '%'  * Ct( Cc(eval) * V 'inject' ),
-    param    = '%'  * Ct( Cc 'param' * (digit^1 / tonumber) ),
-    const    = '%%' * Cc '%'
-             + '%'  * Ct( Cc 'const' * C(P(1)) ),
-    macro    = '%{' * sp0 * '}' * Cc ''
-             + '%{' * sp0 * Ct(name * (sp1 * V 'tokens')^-1) * sp0 * '}'
-             + '%{' * sp0 * Ct(Cc "apply" * V 'tokens') * sp0 * '}'
-             + '%'  * Ct(name),
-    tokens   = V 'token' * (sp1 * V 'token') ^ 0,
-    token    = V 'escape' + quoted + name + number/tonumber,
-}
---}}}
-
-local function read(str) return match(template, str) end
-
-local function read_ast(ast)
-    if type(ast) == 'table' then
-        local op = table.remove(ast, 1)
-        if op == top then
-            local code = "local param = { ... }\nreturn %s"
-            local args = {}
-            for i, x in ipairs(ast) do
-                args[i] = read_ast(x)
-            end
-            return string.format(code, table.concat(args, " .. "))
-        elseif op == eval then
-            local ast = read(ast[1])
-            ast[1] = 'concat'
-            return read_ast(ast)
-        elseif op == 'param' and ast[1] == 0 then
-            return '_concat(param)'
-        elseif op == 'param' then
-            return 'param[' .. read_ast(ast[1]) .. ']'
-        elseif type(ast[1]) == 'table' and ast[1][1] == 'param' and ast[1][2] == 0 then
-            return string.format("%s(...)", op)
-        else
-            local code = '%s(%s)'
-            local args = {}
-            for i, x in ipairs(ast) do
-                args[i] = read_ast(x)
-            end
-            return string.format(code, op, table.concat(args, ", "))
-        end
-    elseif type(ast) == 'string' then
-        if ast == "\n" then
-            return '"\\n"'
-        else
-            return string.format('%q', ast)
-        end
-    elseif type(ast) == 'number' then
-        return ast
-    end
-end
-
+require "moonshine.prelude"
+local parser   = require "moonshine.template.parser"
 local Template = new "moonshine.object"
 
 Template.const = {}
 Template.env = {
-    concat  = function (...) return table.concat({ ... }, " ") end,
-    _concat = function (P) return table.concat(P, " ") end,
+    concat   = function (...) return table.concat({ ... }, "") end,
+    concat_0 = function (P) return table.concat(P, " ") end,
 }
 setmetatable(Template.env, { __index = function () return function () return "" end end })
 
@@ -130,7 +20,7 @@ end
 
 -- add a new Template.
 function Template:define(name, code)
-    local f = loadstring(read_ast(read(code)))
+    local f = loadstring(parser.read(code))
     setfenv(f, self.env)
     self.env[name] = f
 end
@@ -144,9 +34,5 @@ function Template:apply(name, ...)
     self.env[name] = f
     return r
 end
-
---print(read_ast(read("[debug]%| %{style debug}%1")))
---print(read_ast(read("%{debug %(we have %1 colors and %2 styles)}")))
-
 
 return Template
---
+++ runtime/moonshine/template/parser.lua
@@ -0,0 +1,174 @@
+--[[
+    %foo           is a macro call
+    %{foo}         is a macro call
+    %{foo bar baz} is a macro call with arguments.
+
+    Arguments can be:
+        * macro calls
+        * sugar (described below)
+        * quoted strings ("foo" or 'bar')
+        * bare words (foo, foo123, foo_123, foo_bar, _)
+        * numbers (5, 8.4, .3)
+
+    Sugar:
+        * %1         is short for %{param 1}
+        * %%         is a literal '%'
+        * %|         is short for %{const '|'}
+        * %(TEXT)    is special, it causes TEXT to be evaluated as if it was outside of any macro call.
+
+    Literals: Anything that does not begin with a '%' is a literal.
+]]
+-- {{{ lpeg imports
+local lpeg          = require "lpeg"
+local P, R, S, V    = lpeg.P, lpeg.R,  lpeg.S,  lpeg.V
+local C, Ct, Cc, Cs = lpeg.C, lpeg.Ct, lpeg.Cc, lpeg.Cs
+local match         = lpeg.match
+-- }}}
+
+-- {{{ grammar
+local letter  = R('AZ','az') + '_'
+local digit   = R '09'
+local space   = S " \t\r\n"
+local sp0     = space^0
+local sp1     = space^1
+
+local name    = C( letter * (letter + digit)^0 )
+local integer = C( digit^1 ) / tonumber
+local number  = C( (P('-')^-1) * (digit^1 * '.' * digit^1 + digit^1 + '.' * digit^1) ) / tonumber
+local punct   = C( S [=[`~!@#$^&**<>[]-=+|/\?,.]=] )
+local quote   = function(chr)
+    local any = (P ("\\" .. chr) / chr)
+              + (P "\\\\" / "\\")
+              + (P(1) - chr)
+    return chr * Cs( any^0 ) * chr
+end
+local squote = quote([[']])
+local dquote = quote([[']])
+
+function label(n, p)
+    return Ct(Cc(n) * p)
+end
+
+-- special opcodes that cannot be generated in templates:
+local template = P {
+    'root',
+    root    = Ct( Cc 'ROOT' * ((V 'literal' + V 'escape')^1)^-1),
+    literal = C((P(1) - '%')^1),
+    parens  = '(' * sp0 * C(((1 - S '()') + V 'parens')^0) * sp0 * ')',
+    escape  = '%'  * Ct(Cc "APPLY" * name)
+            + '%'  * Ct(Cc "APPLY" * Cc "param" * integer)
+            + '%{' * sp0 * '}'
+            + '%{' * sp0 * Ct(Cc "APPLY" * name * sp0 * V 'tokens') * sp0 * '}'
+            + '%{' * sp0 * Ct(Cc "APPLY" * V 'tokens') * sp0 * '}'
+            + '%'  * Ct(Cc "EVAL" * V 'parens')
+            + '%%' * Cc '%'
+            + '%'  * Ct(Cc "APPLY" * Cc "const" * punct),
+    tokens  = V 'token' * (sp1 * V 'tokens')^-1,
+    token   = name + number + squote + dquote + V 'escape',
+}
+-- }}}
+
+-- parse a string into a basic ast.
+local function parse(text) return match(template, text) end
+
+local TYPE = {}
+local OP   = {}
+
+local function compile(op)
+    local f = assert(TYPE[ type(op) ], "unknown opcode type: " .. type(op))
+    return f(op)
+end
+
+local function compile_ops(ops)
+    local result = {}
+    for i, op in ipairs(ops) do
+        result[i] = compile(op)
+    end
+    return result
+end
+
+function TYPE.string(s) return string.format('%q', s) end
+function TYPE.number(n) return n end
+function TYPE.boolean(b) return tostring(b) end
+
+function TYPE.table(op)
+    local name = table.remove(op, 1)
+    local f = OP[name] or function() return "'no op named "..name.."'" end
+    return f(op)
+end
+
+function OP.ROOT(args)
+    return string.format("local P = { ... }\nreturn %s", table.concat(compile_ops(args), " .. "))
+end
+
+function OP.EVAL(args)
+    local code = args[1]
+    local op = parse(code)
+    op[1] = 'APPLY'
+    table.insert(op, 2, "concat")
+    return compile(op)
+end
+
+local keywords = {
+    'if', 'then', 'while', 'for', 'do', 'until', 'end',
+    'local', 'function', 'return', 'break',
+    'param', 'P',
+}
+local is_keyword = {}
+for i, k in ipairs(keywords) do is_keyword[k] = true end
+
+local function is_param_zero(args)
+    return (
+        #args == 1
+        and type(args[1]) == 'table'
+        and args[1][1] == 'APPLY'
+        and args[1][2] == 'param'
+        and args[1][3] == 0 
+    )
+end
+
+function OP.APPLY(args)
+    local name  = table.remove(args, 1)
+    local args_code
+    if is_param_zero(args) then
+        args_code = '...'
+    else
+        args_code = table.concat(compile_ops(args), ", ")
+    end
+
+    if name == 'param' then
+        if args[1] == 0 then
+            return 'concat_0(P)'
+        else
+            return string.format("P[%s]", compile(args[1]))
+        end
+    elseif type(name) == 'table' then
+        return string.format('apply(%s, %s)', compile(name), args_code)
+    elseif type(name) == 'number' or is_keyword[name] or not string.match(name, "^[%w_]+$") then
+        return string.format('apply(%q, %s)', tostring(name), args_code)
+    else
+        return string.format('%s(%s)', name, args_code)
+    end
+end
+
+local M = {}
+
+function M.read(str)
+    return compile(parse(str))
+end
+
+function M.json(str)
+    require 'json'
+    return json.encode(parse(str))
+end
+
+--[[
+print(M.read("%{debug %(this is the debug text. Level: %1, Power: %2)}"))
+print(M.read("%{debug %{concat 'this is the debug text. Level: ' %1 ', Power: ' %2}}"))
+print(M.read("%{local %{'bat man' 'this is the debug text. Level: ' %1 ', Power: ' %2}}"))
+print(M.read("%{ %foo foo bar}"))
+print(M.read('%{ %(%{prefix}_%1) foo bar}'))
+]]
+
+
+return M
